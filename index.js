9cd5fe017b709f2633d295655a63aaa5c9d784e9
Corey Anderson <48457668+Aqueous20@users.noreply.github.com>
2022-09-20 08:18:38 -0500

    things will be changed

---
+++ index.js
@@ -0,0 +1,59 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20
+
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i =0; i < shapes.length; i++){
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle){
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+    
+}
+
+async function contact(){
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+
+    await emailjs.sendForm(`service_k3l7uhc`, `template_x7lefb21`, event.target, `BK-swYiWlCahKXxpA`) 
+    loading.classList.remove("modal__overlay--visible"), 
+    success.classList += " modal__overlay--visible"
+    
+    try{
+        contact()
+    }
+    catch{
+        loading.classList.remove("modal__overlay--visible") 
+        alert("The email service is temporarily unavailable. Please contact me at coreyandersonjm101@gmail.com");
+    }
+}
+
+
+
+function toggleModal(){   
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
